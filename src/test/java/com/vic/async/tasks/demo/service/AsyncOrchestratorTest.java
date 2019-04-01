b9b0aa19d3750e12f753d832bcb65b8ce00ceeac
victor <victor.prp@gmail.com>
2019-04-01 20:41:15 +0300

    async demo

--- src/test/java/com/vic/async/tasks/demo/service/AsyncOrchestratorTest.java
+++ src/test/java/com/vic/async/tasks/demo/service/AsyncOrchestratorTest.java
@@ -36,4 +36,42 @@ public class AsyncOrchestratorTest {
         Assert.assertTrue("Must contain msg from sourceB", result.msgs.contains("B"));
 
     }
+
+
+    @Test
+    public void shouldReturnWithinTimeoutWithPartialResult(){
+        final long timeout = 100;
+        List<SourceService> sources = new ArrayList<>();
+        SourceService sourceA = mock(SourceService.class);
+        when(sourceA.msgs(any())).then((req) -> {
+            sleep(timeout*2);
+            return new SourceResponse("A");
+        });
+        sources.add(sourceA);
+
+        SourceService sourceB = mock(SourceService.class);
+        when(sourceB.msgs(any())).thenReturn(new SourceResponse("B"));
+        sources.add(sourceB);
+
+        long start = System.currentTimeMillis();
+        Orchestrator orchestrator = new AsyncOrchestrator(sources, timeout);
+
+        OrchRes result = orchestrator.msgs(new OrchReq("test-account"));
+
+        long end = System.currentTimeMillis();
+        long executionTimeMillis = end - start;
+
+        Assert.assertTrue("Must wait for timeout since sourceA is very slow", executionTimeMillis >= timeout && executionTimeMillis < timeout*2);
+        Assert.assertFalse("Must NOT contain msg from sourceA", result.msgs.contains("A"));
+        Assert.assertTrue("Must contain msg from sourceB", result.msgs.contains("B"));
+
+    }
+
+    void sleep(long millis){
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Interrupted",e);
+        }
+    }
 }
